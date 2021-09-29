09f12063fcc26d66261f0188361541424acf9f65
HamzaEjaz420 <hamzaejaz97@gmail.com>
2021-09-29 04:59:16 -0400

    Final Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # on root send req to pages controller home action
-  root "pages#home"
+  
+  # root "pages#home"
+  root "todos#index"
   get "/about", to: "pages#about"
   get "/help", to: "pages#help" 
 
-  resources :todos 
+  resources :todos  
 end
  
